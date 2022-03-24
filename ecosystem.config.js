61c8ef9c5e9d5d799349d4fae962d1036b0142f9
jamalapriadi <jamal.apriadi@gmail.com>
2022-03-24 13:53:36 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'HspFe',
+            exec_mode: 'cluster',
+            instances: 'max', // Or a number of instances
+            script: './node_modules/nuxt/bin/nuxt.js',
+            args: 'start'
+        }
+    ]
+}
