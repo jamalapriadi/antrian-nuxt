0439a9ca96d2f99929220f30529a550b648ba864
jamalapriadi <jamal.apriadi@gmail.com>
2022-04-09 23:10:47 +0700

    add modul pelayanan

--- pages/layar/_id/index.vue
+++ pages/layar/_id/index.vue
@@ -10,13 +10,33 @@
                 <p v-if="list.user" class="text-center">
                     <strong v-if="list.user.user">Dengan User : {{list.user.user.name}}</strong>
                 </p>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="text-center" v-if="list.current_antrian">
+                            <span v-if="list.current_antrian.antrian" style="font-size:80px">{{list.current_antrian.antrian.no_antrian}}</span>
+                        </h1>
+
+                        <div class="text-center">
+                            <p>
+                                Kategori : <span v-if="list.current_antrian.antrian">
+                                    <span v-if="list.current_antrian.antrian.type == 2">UMUM</span>
+                                    <span v-if="list.current_antrian.antrian.type == 1" style="color:red;font-weight:bold">DIFABEL/PRIORITAS</span>
+                                </span>
+                            </p>
+                            <p v-if="list.current_antrian.antrian" style="line-height:5px">
+                                Keperluan : <span v-if="list.current_antrian.antrian.keperluan" style="font-weight:bold;">{{list.current_antrian.antrian.keperluan.nama}}</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div v-if="list.success == false" class="alert alert-danger">
                 {{list.message}}
             </div>
         </div>
-        <pre>{{list}}</pre>
+        <!-- <pre>{{list}}</pre> -->
     </div>
 </template>
 
