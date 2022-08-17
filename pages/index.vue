40ae6ab76007ebe578dce34e4fe521b2f838e8f6
jamalapriadi <jamal.apriadi@gmail.com>
2022-08-17 12:08:45 +0700

    revisi index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,71 @@
 <template>
-  <Tutorial/>
+    <div>
+        <div v-for="(l,idx) in $auth.user.data.role" :key="'per'+idx">
+            <operation v-if="l.name == 'Operation'" />
+            <dashboard v-if="l.name == 'Admin'" />
+        </div>
+        <!-- <pre>{{$auth.user.data}}</pre> -->
+
+        
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+    layout:'main',
+    data(){
+        return {
+            barChartData: {
+                labels: [],
+                datasets: [
+                    {
+                        label: "Visualizaciones",
+                        data: [],
+                        // backgroundColor: "rgba(20, 255, 0, 0.3)",
+                        borderColor: "rgba(100, 255, 0, 1)",
+                        borderWidth: 2,
+                    },
+                ],
+            },
+            barChartOptions: {
+                responsive: true,
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: true,
+                    //   text: "Google analytics data",
+                    fontSize: 24,
+                    fontColor: "#6b7280",
+                },
+                tooltips: {
+                    backgroundColor: "#17BF62",
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            display:false,
+                            gridLines: {
+                                display: false,
+                            },
+                        },
+                    ],
+                    yAxes: [
+                        {
+                            ticks: {
+                                // beginAtZero: true,
+                                // max: 700,
+                                min: 0,
+                                // stepSize: 1,
+                            },
+                            gridLines: {
+                                display: true,
+                            },
+                        },
+                    ],
+                },
+            }
+        }
+    }
 }
 </script>
